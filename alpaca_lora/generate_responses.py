1235cff427ba8e3cedf55b90738416e4ad744786
HeorhiiS <hs3470@nyu.edu>
2023-06-12 20:44:32 +0400

    :honeybee: Adding basic batching to drop inference time

--- alpaca_lora/generate_responses.py
+++ alpaca_lora/generate_responses.py
@@ -16,6 +16,7 @@ import tqdm
 
 def generate(
         model_type: str = "LLAMA-7B",
+        batch_size: int = 10,
 ):
 
     # Load the model
@@ -75,63 +76,75 @@ def generate(
 
     model.eval()
     eval_data = load_dataset("json", data_files="../litllamadata/finetune_dataset/llama_eval_red.json")
-
     precision_scores = []
     progress_bar = tqdm.tqdm(total=len(eval_data['train']), ncols=100, colour='magenta', ascii="░▒█")
+    batched_prompt = []
+    batched_og_output = []
+    counter = 0
+    movie_df = pd.read_json('../movie_map.json')
+    all_titles = movie_df['title']
+    all_titles = np.array(all_titles)
 
     for prompt in eval_data['train']:
+
         instruction = prompt['instruction']
         input = prompt['input']
         og_output = prompt['output']
 
         prompt = f'### Instruction: {instruction}\n ### Input: ' + f"{input}\n ### Output:"
-        input_ids = tokenizer(prompt, return_tensors="pt").input_ids
-        input_ids = input_ids.to('cuda')
+        batched_prompt.append(prompt)
+        batched_og_output.append(og_output)
+        counter += 1
+        if counter % batch_size == 0:
 
-        movie_df = pd.read_json('../movie_map.json')
-        all_titles = movie_df['title']
-        all_titles = np.array(all_titles)
+            input_ids = tokenizer(batched_prompt, return_tensors="pt").input_ids
+            input_ids = input_ids.to('cuda')
 
-        with torch.no_grad():
-            generation_output = model.generate(
-                input_ids=input_ids,
-                repetition_penalty=2.0,
-                max_new_tokens=128,
-                temperature=1,
-                top_p=1,
-                top_k=50,
-                num_beams=20,
-                do_sample=True,
-                eos_token_id=model.config.eos_token_id,
+            with torch.no_grad():
+                generation_output = model.generate(
+                    input_ids=input_ids,
+                    repetition_penalty=2.0,
+                    max_new_tokens=128,
+                    temperature=1,
+                    top_p=1,
+                    top_k=50,
+                    num_beams=20,
+                    do_sample=True,
+                    eos_token_id=model.config.eos_token_id,
 
-            )
-            output = tokenizer.decode(generation_output[0])
-            parsed = output.split("Output:")[1].strip().strip()[:-4].split(", ")
+                )
+                output = tokenizer.batch_decode(generation_output[0])
 
-            parsed_og_output = og_output.strip().strip().split(", ")
+                for output_elem in output:
 
-            fixed_output = []
-            for parsed_title in parsed:
-                fixed_title = find_closest_string(parsed_title, all_titles)
-                fixed_output.append(fixed_title)
+                    parsed = output_elem.split("Output:")[1].split("</s>")[0].strip().split(", ")[:4]
 
-            for row in movie_df.iterrows():
-                title = row[1]['title']
-                mapping = row[1]['movie_id']
+                    parsed_og_output = og_output.strip().split(", ")
 
-                if title in fixed_output:
-                    fixed_output[fixed_output.index(title)] = mapping
-                if title in parsed_og_output:
-                    parsed_og_output[parsed_og_output.index(title)] = mapping
+                    fixed_output = []
+                    for parsed_title in parsed:
+                        fixed_title = find_closest_string(parsed_title, all_titles)
+                        fixed_output.append(fixed_title)
 
-            set_preds = set(fixed_output)
-            set_test = set(parsed_og_output)
+                    for row in movie_df.iterrows():
+                        title = row[1]['title']
+                        mapping = row[1]['movie_id']
 
-            common_elements = set_preds.intersection(set_test)
-            precision = len(common_elements) / len(set_preds)
+                        if title in fixed_output:
+                            fixed_output[fixed_output.index(title)] = mapping
+                        if title in parsed_og_output:
+                            parsed_og_output[parsed_og_output.index(title)] = mapping
 
-            precision_scores.append(precision)
-            progress_bar.update(1)
+                    set_preds = set(fixed_output)
+                    set_test = set(parsed_og_output)
+
+                    common_elements = set_preds.intersection(set_test)
+                    precision = len(common_elements) / len(set_preds)
+
+                    precision_scores.append(precision)
+                    counter = 0
+                    batched_prompt = []
+                    progress_bar.update(1)
 
     # mean average precision
     print(f'Mean Average Precision for {model_type}: {np.mean(precision_scores)}')
